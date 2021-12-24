8d0da1c3a7ab96933fab7be7fbb3d45e71815ac0
Deni Rodrigues <denirodrigues1@gmail.com>
2021-12-24 16:13:13 -0300

    mysterious-organism - done

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -14,25 +14,46 @@ const mockUpStrand = () => {
 };
 
 const pAequorFactory = (specimenNum, dna) => {
-  console.log(dna)
+  console.log(dna);
   return {
     specimenNum,
     dna,
     mutate() {
-      let idx = Math.floor(Math.random() * this.dna.length)
-      let newBase = returnRandBase()
-      
+      let idx = Math.floor(Math.random() * this.dna.length);
+      let newBase = returnRandBase();
+
       if (newBase !== this.dna[idx]) {
-        this.dna.splice(idx, 1, newBase)
-      } 
-      return this.dna
+        this.dna.splice(idx, 1, newBase);
+      }
+      return this.dna;
     },
     compareDna(pAequor) {
-
-    }
+      let percent = 0;
+      let survivalPercent = 0;
+      for (let i = pAequor.length - 1; i >= 0; i--) {
+        for (let a = this.dna.length - 1; a >= 0; a--) {
+          if (pAequor[i] === this.dna[a] && i === a) {
+            percent += 1;
+            if (this.dna[a] === "G" || this.dna[a] === "C")
+              survivalPercent += 1;
+          }
+        }
+      }
+      percent = (percent / pAequor.length) * 100;
+      percent = Math.floor(percent);
+      console.log(
+        `specimen #${this.specimenNum} and specimen #2 have ${percent}% DNA in common`
+      );
+    },
+    willLikelySurvive() {
+      return this.survivalPercent >= 6;
+    },
   };
 };
 
-const magma = pAequorFactory(5, mockUpStrand());
+const strangeAnimal = pAequorFactory(
+  Math.floor(Math.random() * 9),
+  mockUpStrand()
+);
 
-console.log(magma.mutate());
+//console.log(strangeAnimal)
